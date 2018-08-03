a4eb46db2413cc992be362fc30dd4c226532b3f0
madhavparekh <madhavparekh@gmail.com>
2018-08-02 18:42:54 -0700

    BB-21: formated article displayed

--- src/components/newsItem/Title.tsx
+++ src/components/newsItem/Title.tsx
@@ -7,11 +7,30 @@ interface IProps {
 }
 
 const Title: React.SFC<IProps> = ({ type }) => {
+<<<<<<< HEAD
     return (
         <ContentHeader>
             NEWS / MEDIA : {type in newsMap ? newsMap[type] : "Missing Map"}
         </ContentHeader>
     );
+=======
+	let displayType = "";
+	switch (type) {
+		case "media-interviews-reviews":
+			displayType = "Media Interviews & Reviwes";
+			break;
+		case "online-lectures":
+			displayType = "Online Lectures";
+			break;
+		case "video-interviews":
+			displayType = "Video Interviews";
+			break;
+		case "published-articles":
+			displayType = "Published Articles";
+			break;
+	}
+	return <ContentHeader>News/Media : {displayType}</ContentHeader>;
+>>>>>>> BB-21: formated article displayed
 };
 
 export default Title;
