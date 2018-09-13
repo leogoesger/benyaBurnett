1e9795ac96b9829b75449cc36abd0f053662cf04
Leo Q <leoq91@gmail.com>
2018-09-12 22:39:05 -0700

    minor modifications on styling and ordering

--- src/components/home/Articles.tsx
+++ src/components/home/Articles.tsx
@@ -5,53 +5,54 @@ import { Colors } from "../../styles";
 import { Link } from "react-router-dom";
 
 const Articles = () => {
-	return (
-		<div
-			style={{
-				width: "300px",
-				backgroundColor: Colors.GREEN,
-				height: "100%",
-			}}
-		>
-			<div
-				style={{
-					width: "300px",
-					height: "50%",
-					minHeight: "420px",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "space-around",
-					marginTop: "10px",
-				}}
-			>
-				{news.map((article) => {
-					return (
-						<Link to={article.to}>
-							<div
-								className="article-nav-item"
-								style={{
-									color: Colors.WHITE,
-									height: "auto",
-								}}
-							>
-								<span style={{ fontWeight: 400 }}>
-									{article.displayName}
-								</span>
-								<div
-									style={{
-										fontSize: "10px",
-										marginTop: "6px",
-									}}
-								>
-									{article.description}
-								</div>
-							</div>
-						</Link>
-					);
-				})}
-			</div>
-		</div>
-	);
+    return (
+        <div
+            style={{
+                width: "300px",
+                backgroundColor: Colors.GREEN,
+                height: "100%",
+            }}
+        >
+            <div
+                style={{
+                    width: "300px",
+                    height: "50%",
+                    minHeight: "420px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-around",
+                    marginTop: "40px",
+                }}
+            >
+                {news.map(article => {
+                    return (
+                        <Link to={article.to}>
+                            <div
+                                className="article-nav-item"
+                                style={{
+                                    color: Colors.WHITE,
+                                    height: "auto",
+                                    marginTop: "10px",
+                                }}
+                            >
+                                <span style={{ fontWeight: 400 }}>
+                                    {article.displayName}
+                                </span>
+                                <div
+                                    style={{
+                                        fontSize: "10px",
+                                        marginTop: "6px",
+                                    }}
+                                >
+                                    {article.description}
+                                </div>
+                            </div>
+                        </Link>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Articles;
