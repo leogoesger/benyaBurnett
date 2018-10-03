961b34e472dfaec0fa99d5c9812d4caf98d42af9
madhavparekh <madhavparekh@gmail.com>
2018-10-02 22:17:49 -0700

    BB-jwt

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -10,57 +10,53 @@ import { isBrowserNotSupported } from "../utils/helpers";
 interface IProps {}
 
 interface IState {
-	dialogOpen: boolean;
+    dialogOpen: boolean;
 }
 
 class Home extends React.PureComponent<IProps, IState> {
-	constructor(props) {
-		super(props);
-		this.state = {
-			dialogOpen: false,
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            dialogOpen: false,
+        };
+    }
 
-	componentWillMount() {
-		console.log("checking browser didmount");
-		console.log(isBrowserNotSupported());
-		isBrowserNotSupported() ? this.setState({ dialogOpen: true }) : null;
-	}
+    componentWillMount() {
+        isBrowserNotSupported() ? this.setState({ dialogOpen: true }) : null;
+    }
 
-	renderDialogMessage() {
-		console.log("not supported");
-		return (
-			<div>
-				{
-					"Sorry, your browser may not be fully supported! We recommend Chrome v51+, Firefox v51+ or Edge v12+."
-				}
-			</div>
-		);
-	}
+    renderDialogMessage() {
+        return (
+            <div>
+                {
+                    "Sorry, your browser may not be fully supported! We recommend Chrome v51+, Firefox v51+ or Edge v12+."
+                }
+            </div>
+        );
+    }
 
-	handleClose() {
-		this.setState({ dialogOpen: false });
-	}
+    handleClose() {
+        this.setState({ dialogOpen: false });
+    }
 
-	render() {
-		return (
-			<React.Fragment>
+    render() {
+        return (
+            <React.Fragment>
+                <Layout />
+                <Dialog open={this.state.dialogOpen} onClose={this.handleClose}>
+                    {this.renderDialogMessage()}
 
-				<Layout />
-				<Dialog open={this.state.dialogOpen} onClose={this.handleClose}>
-					{this.renderDialogMessage()}
-
-					<div
-						style={{ display: "flex", justifyContent: "flex-end" }}
-					>
-						<Button onClick={() => this.handleClose()}>
-							Continue
-						</Button>
-					</div>
-				</Dialog>
-			</React.Fragment>
-		);
-	}
+                    <div
+                        style={{ display: "flex", justifyContent: "flex-end" }}
+                    >
+                        <Button onClick={() => this.handleClose()}>
+                            Continue
+                        </Button>
+                    </div>
+                </Dialog>
+            </React.Fragment>
+        );
+    }
 }
 
 export default Home;
