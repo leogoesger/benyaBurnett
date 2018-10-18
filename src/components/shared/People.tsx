72fbc5d82f9ce4b327bdb1b6961578e20a3ee1d3
madhavparekh <madhavparekh@gmail.com>
2018-10-18 10:14:00 -0700

    BB-IE web link fixes

--- src/components/shared/People.tsx
+++ src/components/shared/People.tsx
@@ -3,82 +3,84 @@ import * as React from "react";
 import { Colors } from "../../styles";
 
 interface IProps {
-    member: {
-        company: string;
-        name: string;
-        website: string;
-        address: string;
-        phone: string;
-        image: string;
-    };
+	member: {
+		company: string;
+		name: string;
+		website: string;
+		address: string;
+		phone: string;
+		image: string;
+	};
 }
 
 const People: React.SFC<IProps> = ({ member }) => {
-    return (
-        <div style={{ display: "flex" }}>
-            <img src={member.image} width="180px" height="220" />
-            <div
-                style={{
-                    padding: "10px",
-                    lineHeight: "20px",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                }}
-            >
-                <div>
-                    <div>{member.company}</div>
-                    <div style={{ fontWeight: 400 }}>{member.name}</div>
-                    <div style={{ marginTop: "10px" }}>
-                        <a href={member.website}>{member.website}</a>
-                    </div>
-                    <div style={{ marginTop: "10px" }}>{member.address}</div>
-                    <div>{member.phone}</div>
-                </div>
-                <div>
-                    <div
-                        style={{
-                            cursor: "pointer",
-                            height: "20px",
-                            color: "#e0e0e0",
-                            backgroundColor: Colors.GREEN,
-                            borderRadius: "2px",
-                            textAlign: "center",
-                            fontSize: "13px",
-                            marginTop: "20px",
-                        }}
-                    >
-                        Download CV
-                    </div>
-                    <div
-                        style={{
-                            cursor: "pointer",
-                            height: "20px",
-                            color: "#e0e0e0",
-                            backgroundColor: Colors.OFFGREEN,
-                            borderRadius: "2px",
-                            textAlign: "center",
-                            fontSize: "13px",
-                            marginTop: "10px",
-                        }}
-                    >
-                        Download Bio
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div style={{ display: "flex" }}>
+			<img src={member.image} width="180px" height="220" />
+			<div
+				style={{
+					padding: "10px",
+					lineHeight: "20px",
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "space-between",
+				}}
+			>
+				<div>
+					<div>{member.company}</div>
+					<div style={{ fontWeight: 400 }}>{member.name}</div>
+					<div style={{ marginTop: "10px" }}>
+						<a href={member.website} target="_blank">
+							{member.website.split("www.")[1]}
+						</a>
+					</div>
+					<div style={{ marginTop: "10px" }}>{member.address}</div>
+					<div>{member.phone}</div>
+				</div>
+				<div>
+					<div
+						style={{
+							cursor: "pointer",
+							height: "20px",
+							color: "#e0e0e0",
+							backgroundColor: Colors.GREEN,
+							borderRadius: "2px",
+							textAlign: "center",
+							fontSize: "13px",
+							marginTop: "20px",
+						}}
+					>
+						Download CV
+					</div>
+					<div
+						style={{
+							cursor: "pointer",
+							height: "20px",
+							color: "#e0e0e0",
+							backgroundColor: Colors.OFFGREEN,
+							borderRadius: "2px",
+							textAlign: "center",
+							fontSize: "13px",
+							marginTop: "10px",
+						}}
+					>
+						Download Bio
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default People;
 
 const btnStyle = {
-    cursor: "pointer",
-    height: "20px",
-    color: "#e0e0e0",
-    backgroundColor: Colors.OFFGREEN,
-    borderRadius: "2px",
-    textAlign: "center",
-    paddingTop: "6px",
-    fontSize: "13px",
+	cursor: "pointer",
+	height: "20px",
+	color: "#e0e0e0",
+	backgroundColor: Colors.OFFGREEN,
+	borderRadius: "2px",
+	textAlign: "center",
+	paddingTop: "6px",
+	fontSize: "13px",
 };
