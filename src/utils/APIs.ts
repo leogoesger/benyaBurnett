faadd102b034d0accdb9527c36445e1d230de595
madhavparekh <madhavparekh@gmail.com>
2018-10-17 17:08:51 -0700

    BB-jwt

--- src/utils/APIs.ts
+++ src/utils/APIs.ts
@@ -2,37 +2,38 @@ import axios from "axios";
 import { AxiosResponse } from "axios";
 
 axios.defaults.headers.common["Authorization"] = localStorage.getItem(
-    "bbToken"
+	"bbToken"
 );
 
 const url = `${process.env.SERVER_ADDRESS}/api`;
 
 interface IArticle {
-    title: string;
-    text: string;
-    url: string;
-    date: string;
-    imgUrl: string;
-    type: string;
-    _id: string;
+	title: string;
+	text: string;
+	url: string;
+	date: string;
+	imgUrl: string;
+	type: string;
+	_id: string;
 }
 
-export const createArticle = data => {
-    return axios
-        .post(`${url}/articles/post`, data)
-        .catch(e => localStorage.removeItem("Authorization"));
+export const createArticle = (data) => {
+	return axios
+		.post(`${url}/articles/post`, data)
+		.catch((e) => localStorage.removeItem("bbToken"));
 };
 
 export const deleteArticle = (articleId: string) => {
-    return axios
-        .delete(
-            `${process.env.SERVER_ADDRESS}/api/articles/remove/${articleId}`
-        )
-        .catch(e => localStorage.removeItem("Authorization"));
+	return axios
+		.delete(
+			`${process.env.SERVER_ADDRESS}/api/articles/remove/${articleId}`
+		)
+		.catch((e) => localStorage.removeItem("bbToken"));
 };
 
-export const updateArticle = data => {
-    return axios
-        .put(`${process.env.SERVER_ADDRESS}/api/articles/update`, data)
-        .then((article: any) => article);
+export const updateArticle = (data) => {
+	return axios
+		.put(`${process.env.SERVER_ADDRESS}/api/articles/update`, data)
+		.then((article: any) => article)
+		.catch((e) => localStorage.removeItem("bbToken"));
 };
